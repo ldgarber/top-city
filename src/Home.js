8af875682d70ea74b6af2fead528c787aa016935
ldgarber <leah.garber88@gmail.com>
2018-01-25 12:30:46 -0500

    add text over image on home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ class Home extends Component {
   render() {
     return (
       <div className="page">
-        <img src={"/images/top-city.jpg"} alt="top city"/>
+        <div className="container">
+          <img src={"/images/top-city.jpg"} alt="top city"/>
+          <div className="center-text">A NEIGHBORHOOD JOINT</div>
+        </div>
       </div>
     );  
   }
